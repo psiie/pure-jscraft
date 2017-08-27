59b02c759504a9dd22658f1f597bbb3d3919236f
darkenvy <darkenvy6@gmail.com>
2017-08-27 09:56:54 -0700

    perlin noise land generation with water

--- init.js
+++ init.js
@@ -6,18 +6,59 @@ function init() {
     return pick;
   };
 
-  for (worldX = 0; worldX < 64; worldX++) {
-    for (worldY = 0; worldY < 64; worldY++) {
-      for (worldZ = 0; worldZ < 64; worldZ++) {
-        if (worldY === 36) map[worldX][worldY][worldZ] = 1;
-        // if (worldY === 35 && worldX === 20) map[worldX][worldY][worldZ] = 7;
+  const perlinLandGen = function() {
+    const pn = new Perlin("fjlakj3kn4kj9uvd98vf");
+    const heightMap = new Array(64);
+    for (let x = 0; x < 64; x++) {
+      heightMap[x] = new Array(64);
+      for (let z = 0; z < 64; z++) {
+        let height = pn.noise(x / 15, z / 15, Math.PI);
+        height *= 32;
+        height += 32;
+        height = Math.floor(height);
+        heightMap[x][z] = height;
       }
     }
-  }
-  map[35][35][32] = 2;
-  map[35][34][32] = 2;
-  map[35][33][32] = 2;
-  map[35][32][32] = 1;
+    console.log(heightMap);
+
+    for (let x = 0; x < 64; x++) {
+      for (let z = 0; z < 64; z++) {
+        let mHeight = heightMap[x][z];
+        map[x][mHeight][z] = 1;
+        // water fill 9
+        for (let y = mHeight - 1; y > 50; y--) {
+          map[x][y][z] = 9;
+        }
+
+        // underground fill
+        for (let y = mHeight + 1; y < 64; y++) {
+          map[x][y][z] = 4;
+        }
+      }
+    }
+
+    console.log("done setting perlin");
+  };
+  // for (worldX = 0; worldX < 64; worldX++) {
+  //   for (worldY = 0; worldY < 64; worldY++) {
+  //     for (worldZ = 0; worldZ < 64; worldZ++) {
+  //       // if (worldY === 36) map[worldX][worldY][worldZ] = 1;
+
+  //       let pnHeight = pn.noise(worldX / 64, worldY / 64, 0.5);
+  //       pnHeight *= 32;
+  //       pnHeight += 20;
+  //       if (worldY === (pnHeight | 0)) {
+  //         console.log("yes");
+  //         map[worldX][worldY][worldZ] = 1;
+  //       }
+  //     }
+  //   }
+  // }
+  perlinLandGen();
+  // map[35][35][32] = 3;
+  // map[35][34][32] = 4;
+  // map[35][33][32] = 5;
+  // map[35][32][32] = 6;
 
   const printMap = function() {
     for (worldX = 0; worldX < 64; worldX++) {
